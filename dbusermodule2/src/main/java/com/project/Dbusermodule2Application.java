d22591d20d57bb5f176a8acd23a4c85c52075714
Sathish <sathish@gmail.com>
2025-03-10 15:26:47 +0530

    added openfeign

--- dbusermodule2/src/main/java/com/project/Dbusermodule2Application.java
+++ dbusermodule2/src/main/java/com/project/Dbusermodule2Application.java
@@ -2,8 +2,12 @@ package com.project;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.openfeign.EnableFeignClients;
 
 @SpringBootApplication
+@EnableDiscoveryClient
+@EnableFeignClients
 public class Dbusermodule2Application {
 
 	public static void main(String[] args) {
--- dbusermodule2/src/main/java/com/project/controller/UserController.java
+++ dbusermodule2/src/main/java/com/project/controller/UserController.java
@@ -11,6 +11,7 @@ import org.springframework.security.core.Authentication;
 import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.web.bind.annotation.*;
 
+@RequestMapping("/user")
 @RestController
 public class UserController {
 
