8054d628fc77059d35c66c45ae4a25086d6336bc
Thamizhini <dthamizhini4@gmail.com>
2025-03-14 15:43:41 +0530

    Controller testing

--- DbsInventoryModule/src/main/java/com/project/global/GlobalExceptionHandler.java
+++
@@ -1,9 +0,0 @@
-package com.project.global;
-
-import org.springframework.http.HttpStatus;
-import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.ExceptionHandler;
-
-public class GlobalExceptionHandler {
-    }
-}
