fa9d599bca09667d90944f36500ba74ec87d1f36
Sabarish <>
2025-04-21 11:24:08 +0530

    Show and delete in ReviewDelete

--- ReviewAndRatingModule/src/main/java/com/project/service/ReviewService.java
+++ ReviewAndRatingModule/src/main/java/com/project/service/ReviewService.java
@@ -10,10 +10,12 @@ public interface ReviewService {
 	List<ReviewDTO> retrieveAllReviews() throws ReviewNotFoundException, ServiceUnavailableException;
 	List<ReviewDTO> retrieveAllReviewsByUserId(long userId) throws ReviewNotFoundException, ServiceUnavailableException;
 	List<ReviewDTO> retrieveAllReviewsByBookId(String bookId) throws ReviewNotFoundException, ServiceUnavailableException;
+	List<ReviewDTO> retrieveAllReviewDeletes() throws ServiceUnavailableException;
 	ReviewDTO retrieveReviewById(long reviewId) throws ReviewNotFoundException, ServiceUnavailableException;
 	ReviewDTO addReview(float rating, String comment, long userId, String bookId) throws UserNotFoundException, BookNotFoundException, ServiceUnavailableException;
 	ReviewDTO updateReview(long userId, ReviewDTO reviewDTO) throws UserNotAuthorizedException, UserNotFoundException, IDMismatchException, BookNotFoundException, ServiceUnavailableException;
 	boolean deleteReview(long userId, long reviewId) throws ReviewNotFoundException, UserNotFoundException, UserNotAuthorizedException, ServiceUnavailableException;
-	boolean addToReviewDelete(long reviewId, String comment);
+	boolean addToReviewDelete(long reviewId, String reason);
+	boolean deleteReviewDelete(long userId, long reviewDeleteId) throws ReviewNotFoundException, ServiceUnavailableException, UserNotFoundException, UserNotAuthorizedException;
 	List<Float> retrieveAverageRating(String bookId);
 }
